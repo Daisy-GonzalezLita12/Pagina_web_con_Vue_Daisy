982b1e34b57c7fe556a51e71d7e1a94a37335710
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-03-24 17:08:45 -0600

    Agregando el primer componente card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup>
+import card from '../components/card.vue'
 import { ref } from 'vue'
 
 defineProps({
@@ -23,7 +24,7 @@ const count = ref(0)
             <!-- Imagen de un gatito -->
             <img src="https://i.pinimg.com/474x/29/41/b0/2941b0a266c0fc1cb388429315b9ccf9.jpg" alt="Gato" width="150px" height="200px"> 
             <!-- Descripción de los gatos -->
-            <p class="card-text">El gato doméstico (Felis silvestris catus), llamado comúnmente gato, y de forma coloquial minino, michino, y algunos nombres más, es un mamífero carnívoro de la familia Felidae.</p>
+            <p class="card-text">El gato doméstico (Felis silvestris catus), llamado comúnmente gato, es un mamífero carnívoro de la familia Felidae.</p>
           </div>
         </div>
        </div>
@@ -42,47 +43,11 @@ const count = ref(0)
        </div>
    </div>
    <!-- Cierre del  primer row-->
+    
+   <!--Mandamos a llamar el componente card -->
+   <!-- card1 -->
+    <card/>
 
-   <!-- Inicio del segundo row -->
-    <div class="row mt-4" >
-      <div class="col-md-4">
-         <div class="card" style="width: 18rem;">
-          <!-- Imagen de un gatito -->
-              <img src="https://static.grupojoly.com/clip/17b4e808-da4b-4089-912a-38897c71e4f2_source-aspect-ratio_1600w_0.jpg" class="card-img-top" alt="Gato"  width="350px" height="180px" >
-          <div class="card-body text-center">
-            <!--Descripción del tamaño de los gatos  -->
-              <p class="card-text">Miden alrededor de 46 cm de longitud de la cabeza al cuerpo y entre 23-25 cm de altura.</p>
-              <!-- Botón de "Más información" -->
-              <button class=" btn btn-warning">Más información</button>
-           </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-         <div class="card" style="width: 18rem;">
-          <!-- Imagen de gatos -->
-              <img src="https://buzos.com.mx/storage/public/fotogaleria/noticias/2024-08-08/NncgcyFt0C.webp" class="card-img-top" alt="Gatos"  width="350px" height="180px">
-          <div class="card-body text-center">
-            <!-- Decripción de la población de los gatos -->
-              <p class="card-text">En 2017, la población mundial estimada de gatos estaba en seiscientos millones de felinos.</p>
-             <!-- Botón de "Más información" -->
-              <button class=" btn btn-warning">Más información</button>
-           </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-         <div class="card" style="width: 18rem;">
-          <!-- Imagen de un perro con un señor -->
-              <img src="https://www.animalshealth.es/fileuploads/news/perro-propietario-europa-aumento-tenencia-217031581651.jpg" class="card-img-top" alt="Perro" width="350px" height="180px">
-          <div class="card-body text-center">
-            <!-- Descripción de los perros en Europa -->
-              <p class="card-text">El número de perros como mascota en Europa se ha incrementado en más de 11 de millones.</p>
-              <!-- Botón de "Más información" -->
-              <button class=" btn btn-warning">Más información</button>
-           </div>
-        </div>
-      </div>
-    </div>
-    <!-- Cierre del segundo wow -->
   </div>
 </template>
 <style scoped>
